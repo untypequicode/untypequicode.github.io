f6f0b3fed77544f881c147b9cbb9cf4bdd5c1279
untypequicode <untypequicode@gmail.com>
2023-06-16 23:18:01 +0200

    update animation and finish responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section');
@@ -24,4 +31,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbars links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
