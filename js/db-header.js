d9aa65282d3f6de06458e78abf6ba4ebfe2bbbe0
untypequicode <untypequicode@gmail.com>
2024-04-06 23:45:54 +0200

    First major changes to the portfolio. No complete version available yet.

---
+++ js/db-header.js
@@ -0,0 +1,23 @@
+const db_header = [
+    {
+        text: "Accueil",
+        url: "https://untypequicode.github.io/",
+    },
+    {
+        text: "Projets",
+        url: "projets",
+    },
+    {
+        text: "À propos",
+        url: "a-propos",
+    },
+];
+
+const logo = {
+    type: "text",
+    // type: "image",
+    content: "untypequicode.",
+    // content: "images/favicon.svg",
+    link: "https://untypequicode.github.io/whatsong/",
+    index: "Accueil",
+}
