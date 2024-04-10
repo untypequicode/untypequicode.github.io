5c4d299b8d4ec5afdef137ca4f0a56b868a11cc8
untypequicode <untypequicode@gmail.com>
2024-04-10 14:28:33 +0200

    Adding the projects page content.

---
+++ js/db-projets.js
@@ -0,0 +1,12 @@
+const dbProjets = [
+{
+    id: 1,
+    titre: "Mayday",
+    start_date: "2024-03-24",
+    end_date: "now",
+    client: "Mayday Festival",
+    description: "Site web pour le festival Mayday",
+    image: "img/projets/mayday.png",
+    url: "https://maydayfestival.fr/",
+}
+];
