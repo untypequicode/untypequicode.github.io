1151e0617aed205e0afcf3d4f1525d993c204b93
untypequicode <untypequicode@gmail.com>
2024-05-08 03:13:44 +0200

    Update of the projects page with the addition of all projects. This will require setting up a system of multiple links to insert the GitHub link and other if needed content.

--- js/db-projets.js
+++ js/db-projets.js
@@ -1,12 +1,130 @@
 const dbProjets = [
-{
-    id: 1,
-    titre: "Mayday",
-    start_date: "2024-03-24",
-    end_date: "now",
-    client: "Mayday Festival",
-    description: "Site web pour le festival Mayday",
-    image: "img/projets/mayday.webp",
-    url: "https://maydayfestival.fr/",
-}
+    {
+        id: 1,
+        titre: "Mayday Festival",
+        start_date: "2024-03-24",
+        end_date: "now",
+        client: "M-Tech",
+        description: "Site web pour le festival Mayday",
+        image: "images/projects/mayday.webp",
+        url: "https://maydayfestival.fr/",
+    },
+    {
+        id: 2,
+        titre: "LumiWood",
+        start_date: "2022-08-01",
+        end_date: "2023-06-30",
+        client: "Personnel",
+        description: "LumiWood est une lampe connectée en bois équipée de 100 LED, d'un petit écran et de capteurs tactiles. " +
+            "Créez vos propres animations lumineuses grâce à la matrice de LED, " +
+            "connectez-vous à Internet et utilisez la lampe sans fil grâce à sa batterie rechargeable. " +
+            "Apportez de la lumière et de la chaleur à n'importe quel espace avec LumiWood.",
+        image: "images/projects/lumiwood.webp",
+        url: "https://github.com/Iuris-tech/LumiWood",
+    },
+    {
+        id: 3,
+        titre: "Ekoroji",
+        start_date: "2023-01-01",
+        end_date: "2023-04-30",
+        client: "Trophées NSI",
+        description: "L'origine de ce projet découle du constat que de nombreuses personnes ne savent pas comment trier leurs déchets correctement, " +
+            "ce qui a un impact négatif sur l'environnement. " +
+            "Nous avons donc voulu créer une solution qui permettrait à chacun de devenir un écocitoyen responsable et de contribuer à la protection de notre planète.",
+        image: "images/projects/ekoroji.webp",
+        url: "https://github.com/Les-4-fantastiques/Ekoroji",
+    },
+    {
+        id: 4,
+        titre: "Jumping Hell",
+        start_date: "2021-08-27",
+        end_date: "2021-08-30",
+        client: "Trijam",
+        description: "Dans JumpingHell, saute de plateforme en plateforme. " +
+            "Si tu tombes, tu atterris en enfer. " +
+            "Tu dois alors t'échapper rapidement et revenir à la vie. " +
+            "Garde le cap et essais de ne jamais tomber, sinon à tes risques et périls…",
+        image: "images/projects/jumping-hell.webp",
+        url: "https://github.com/untypequicode/Jumping-Hell",
+    },
+    {
+        id: 5,
+        titre: "Let Me Diiiiie",
+        start_date: "2021-08-27",
+        end_date: "2021-08-30",
+        client: "Trijam",
+        description: "Un seul but, mourir. " +
+            "Dans LetMeDiiiiie, entre la vie et la mort, " +
+            "le seul moyen de gagner est d'être tué par Cerber, " +
+            "le monstre à trois têtes de singe. " +
+            "Ce ne sera pas de tout répit, Cerber est très malin, alors à vous de jouer !",
+        image: "images/projects/let-me-die.webp",
+        url: "https://github.com/untypequicode/Let-Me-Diiiiie",
+    },
+    {
+        id: 6,
+        titre: "Circus Claw Machine",
+        start_date: "2024-01-26",
+        end_date: "2024-01-28",
+        client: "Global Game Jam",
+        description: "Dans le cadre de la \"Global Game Jam 2024\". " +
+            "The Circus Claw Machine, est un jeu de plateforme qui comprend un réglage dans le menu Jeux. " +
+            "Pour lancer le jeu, il faut aussi réussir à prendre les bons éléments de la fameuse machine à pince.",
+        image: "images/projects/circus-claw-machine.webp",
+        url: "https://github.com/untypequicode/circus-claw-machine",
+    },
+    {
+        id: 7,
+        titre: "Green Growth",
+        start_date: "2023-12-07",
+        end_date: "2023-12-08",
+        client: "Nuit de l'info",
+        description: "Green Growth est un site web réalisé durant la nuit de l'info en 16H32 par notre équipe de cinq membres. " +
+            "La version en ligne est celle rendue lors du concours, une mise à jour de type correctif est prévue.",
+        image: "images/projects/green-growth.webp",
+        url: "https://les-codeurs-lunaires.github.io/Green-Growth/",
+    },
+    {
+        id: 8,
+        titre: "Ollama Assist",
+        start_date: "2024-03-13",
+        end_date: "now",
+        client: "Personnel",
+        description: "Ollama Assist : Assistants alimentés par l'IA pour Linux.",
+        image: "images/projects/ollama-assist.svg",
+        url: "https://github.com/untypequicode/ollama-assist",
+    },
+    {
+        id: 9,
+        titre: "Libre Shield",
+        start_date: "2024-01-03",
+        end_date: "now",
+        client: "Personnel",
+        description: "Ici, je partage mes astuces et découvertes pour une vie numérique plus sécurisée. " +
+            "Suivez mon aventure avec LibreShield, des conseils pratiques et des solutions open source.",
+        image: "images/projects/libre-shield.webp",
+        url: "https://github.com/untypequicode/libre-shield",
+    },
+    {
+        id: 10,
+        titre: "Script.js",
+        start_date: "2024-04-05",
+        end_date: "now",
+        client: "Personnel",
+        description: "Script.js est un framework JavaScript minimaliste pour créer des sites web modernes et dynamiques. " +
+            "Il est conçu pour être simple, léger et facile à utiliser.",
+        image: "images/projects/scriptjs.webp",
+        url: "https://github.com/untypequicode/script.js",
+    },
+    {
+        id: 11,
+        titre: "Style.css",
+        start_date: "2024-04-02",
+        end_date: "now",
+        client: "Personnel",
+        description: "Style.css est une bibliothèque CSS minimaliste pour styliser vos sites web. " +
+            "Il est conçu pour être simple, léger et facile à utiliser.",
+        image: "images/projects/stylecss.webp",
+        url: "https://github.com/untypequicode/style.css",
+    },
 ];
